86a88a48725e1883944319f760469e1d8657ca91
Assadullah <assad.hossaini@gmail.com>
2025-01-02 09:52:58 +0430

    refines

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import HomePage from './pages/HomePage';
-import EarnPage from './pages/EarnPage';
-import MemepadPage from './pages/MemepadPage';
-import FrensPage from './pages/FrensPage';
-import WalletPage from './pages/WalletPage';
-import StarField from './components/background/StarField';
-import './styles/animations.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import EarnPage from "./pages/EarnPage";
+import MemepadPage from "./pages/MemepadPage";
+import FrensPage from "./pages/FrensPage";
+import WalletPage from "./pages/WalletPage";
+import StarField from "./components/background/StarField";
+import "./styles/animations.css";
 
 function App() {
   return (
-    <Router basename="/mr2-mini-app">
+    <Router>
       <div className="min-h-screen bg-black text-white relative">
         <StarField />
         <Layout>
           <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/mr2-mini-app" element={<HomePage />} />
             <Route path="/earn" element={<EarnPage />} />
             <Route path="/memepad" element={<MemepadPage />} />
             <Route path="/frens" element={<FrensPage />} />
